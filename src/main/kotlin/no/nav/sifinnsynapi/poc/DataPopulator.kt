786700d40027f22167c0e8dd9066b38d83a37a41
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2020-06-17 10:27:59 +0200

    Legger til testdata for eksisterende bruker i q.

--- src/main/kotlin/no/nav/sifinnsynapi/poc/DataPopulator.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/poc/DataPopulator.kt
@@ -405,6 +405,106 @@ class DataPopulator(
                                   "beskrivelseOmsorgsrollen": "En kort beskrivelse"
                                 }
                             """.trimIndent()
+                    ),
+                    SøknadDAO(
+                            saksId = "sak-5674839201",
+                            aktørId = AktørId.valueOf("1157852636281"),
+                            fødselsnummer = Fødselsnummer.valueOf("27118921009"),
+                            journalpostId = "68493021",
+                            søknadstype = Søknadstype.PP_SYKT_BARN,
+                            status = SøknadsStatus.MOTTATT,
+                            søknad =
+                            //language=json
+                            """
+                                {
+                                    "språk": null,
+                                    "søknadId": "cc644d91-4eff-4679-a7d3-fc57a0c6a1c2",
+                                    "mottatt": "2018-01-02T03:04:05.000000006Z",
+                                    "fraOgMed": "2018-01-01",
+                                    "tilOgMed": "2018-02-02",
+                                    "vedleggUrls": [
+                                        "http://localhost:8080/1234", "http://localhost:8080/12345"],
+                                    "søker": {
+                                        "aktørId": "1157852636281",
+                                        "fødselsnummer": "27118921009",
+                                        "fornavn": "Robust",
+                                        "mellomnavn": "",
+                                        "etternavn": "STAFFELI"
+                                    },
+                                    "barn": {
+                                        "fødselsnummer": "2323",
+                                        "navn": "Kari",
+                                        "fødselsdato": null,
+                                        "aktørId": null
+                                    },
+                                    "relasjonTilBarnet": "Mor",
+                                    "arbeidsgivere": {
+                                        "organisasjoner": [{
+                                            "organisasjonsnummer": "1212",
+                                            "navn": "Nei",
+                                            "skalJobbe": "nei",
+                                            "jobberNormaltTimer": 0.0,
+                                            "skalJobbeProsent": 0.0,
+                                            "vetIkkeEkstrainfo": null
+                                        },{
+                                            "organisasjonsnummer": "54321",
+                                            "navn": "Navn",
+                                            "skalJobbe": "redusert",
+                                            "skalJobbeProsent": 22.512,
+                                            "vetIkkeEkstrainfo": null,
+                                            "jobberNormaltTimer": 0.0
+                                        }]
+                                    },
+                                    "medlemskap": {
+                                        "harBoddIUtlandetSiste12Mnd": true,
+                                        "utenlandsoppholdNeste12Mnd": [],
+                                        "skalBoIUtlandetNeste12Mnd": true,
+                                        "utenlandsoppholdSiste12Mnd": []
+                                    },
+                                    "harMedsøker": true,
+                                    "samtidigHjemme": null,
+                                    "bekrefterPeriodeOver8Uker": true,
+                                    "harBekreftetOpplysninger" : true,
+                                    "harForståttRettigheterOgPlikter": true,
+                                    "tilsynsordning": {
+                                        "svar": "ja",
+                                        "ja": {
+                                            "mandag": "PT5H",
+                                            "tirsdag": "PT4H",
+                                            "onsdag": "PT3H45M",
+                                            "torsdag": "PT2H",
+                                            "fredag": "PT1H30M",
+                                            "tilleggsinformasjon": "Litt tilleggsinformasjon."
+                                        },
+                                        "vetIkke": null
+                                    },
+                                    "beredskap": {
+                                        "beredskap": true,
+                                        "tilleggsinformasjon": "I Beredskap"
+                                    },
+                                    "nattevåk": {
+                                        "harNattevåk": true,
+                                        "tilleggsinformasjon": "Har Nattevåk"
+                                    },
+                                     "utenlandsoppholdIPerioden": {
+                                        "skalOppholdeSegIUtlandetIPerioden": false,
+                                        "opphold": []
+                                    },
+                                  "ferieuttakIPerioden": {
+                                    "skalTaUtFerieIPerioden": false,
+                                    "ferieuttak": [
+                                    ]
+                                  },
+                                    "frilans": {
+                                      "startdato": "2018-02-01",
+                                      "jobberFortsattSomFrilans": true
+                                    },
+                                    "selvstendigVirksomheter" : [],
+                                  "skalBekrefteOmsorg": true,
+                                  "skalPassePaBarnetIHelePerioden": true,
+                                  "beskrivelseOmsorgsrollen": "En kort beskrivelse"
+                                }
+                            """.trimIndent()
                     )
             ).map {
                 log.info("Lagrer søknad: {}", it)
