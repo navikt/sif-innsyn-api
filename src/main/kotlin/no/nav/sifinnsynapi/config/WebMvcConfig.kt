f91f7b5cdf55cf9388f21c4b255e8d81661267e3
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2020-06-26 11:10:07 +0200

    Konfigurerer CORS

---
+++ src/main/kotlin/no/nav/sifinnsynapi/config/WebMvcConfig.kt
@@ -0,0 +1,35 @@
+package no.nav.sifinnsynapi.config
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@EnableWebMvc
+class WebMvcConfig(
+        @Value("\${no.nav.security.cors.allowed-origins}") val allowedOrigins: String
+): WebMvcConfigurer {
+
+    companion object {
+        val log: Logger = LoggerFactory.getLogger(WebMvcConfigurer::class.java)
+    }
+
+    init {
+        log.info("Konfigurerer CORS...")
+        log.info("--- ALLOWED_ORIGINS={}", allowedOrigins)
+    }
+
+    /**
+     * Configure cross origin requests processing.
+     * @since 4.2
+     */
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+                .allowedOrigins(*allowedOrigins.split(",").toTypedArray())
+        super.addCorsMappings(registry)
+    }
+}
