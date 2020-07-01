f4bbb7f7a17b521ec6ed5126bb6dd3db43dd4bf9
Sander Opperud Odden <sander.opperud.odden@nav.no>
2020-07-01 13:04:24 +0200

    Legger til jsonFormat

--- src/main/kotlin/no/nav/sifinnsynapi/poc/SøknadDTO.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/poc/SøknadDTO.kt
@@ -1,5 +1,6 @@
 package no.nav.sifinnsynapi.poc
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import no.nav.sifinnsynapi.common.SøknadsStatus
 import no.nav.sifinnsynapi.common.Søknadstype
 import java.time.LocalDate
@@ -11,7 +12,7 @@ data class SøknadDTO(
         val søknad: Map<String, Any>,
         val saksId: String?,
         val journalpostId: String,
-        val opprettet: LocalDateTime? = null,
-        val endret: LocalDateTime? = null,
-        val behandlingsdato: LocalDate? = null
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss") val opprettet: LocalDateTime? = null,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss") val endret: LocalDateTime? = null,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss") val behandlingsdato: LocalDate? = null
 )
--- src/test/kotlin/no/nav/sifinnsynapi/config/ObjectMapperConfig.kt
+++ src/test/kotlin/no/nav/sifinnsynapi/config/ObjectMapperConfig.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.databind.util.StdDateFormat
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
@@ -16,6 +17,7 @@ class ObjectMapperConfig {
     @Bean( name = ["testMapper"])
     fun configureMapper(): ObjectMapper = ObjectMapper().apply {
         configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+        dateFormat = StdDateFormat().withColonInTimeZone(true)
         configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
         configure(SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS, false)
         propertyNamingStrategy = PropertyNamingStrategy.LOWER_CAMEL_CASE
