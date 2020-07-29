e9dcc43b682982c20577fdf2bc78859531c7de16
Sander Opperud Odden <sander.opperud.odden@nav.no>
2020-07-29 15:24:53 +0200

    Endrer feilmelding

--- src/main/kotlin/no/nav/sifinnsynapi/ExceptionHandler.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/ExceptionHandler.kt
@@ -3,7 +3,6 @@ package no.nav.sifinnsynapi
 import no.nav.security.token.support.spring.validation.interceptor.JwtTokenUnauthorizedException
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -16,26 +15,28 @@ import org.springframework.web.servlet.NoHandlerFoundException
 class ExceptionHandler {
 
     companion object{
-        private val log: Logger = LoggerFactory.getLogger(ExceptionHandler::class.java)
+        private val logger: Logger = LoggerFactory.getLogger(ExceptionHandler::class.java)
     }
 
     @ExceptionHandler(value = [Exception::class])
-    fun håndtereGeneriskException(ex: Exception, request: WebRequest?): ResponseEntity<Any> {
-        log.error("Exception kastet -------> {}, {}, {}, {}", ex.message, HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request.toString())
-        return ResponseEntity("Generisk feilmelding", HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR)
+    fun håndtereGeneriskException(exception: Exception, request: WebRequest?): ResponseEntity<Any> {
+        return logAndHandle(HttpStatus.UNAUTHORIZED, exception, request)
     }
 
     @ExceptionHandler(value = [NoHandlerFoundException::class])
     @ResponseStatus(value= HttpStatus.NOT_FOUND)
-    fun håndtere404Exception(ex: Exception, request: WebRequest?): ResponseEntity<Any> {
-        log.error("Exception kastet -------> {}, {}, {}, {}", ex.message, HttpHeaders(), HttpStatus.NOT_FOUND, request.toString())
-        return ResponseEntity("404 - Not found feilmelding", HttpHeaders(), HttpStatus.NOT_FOUND)
+    fun håndtere404Exception(exception: Exception, request: WebRequest?): ResponseEntity<Any> {
+        return logAndHandle(HttpStatus.UNAUTHORIZED, exception, request)
     }
 
     @ExceptionHandler(value = [JwtTokenUnauthorizedException::class])
-    fun håndtereTokenUnauthorizedException(ex: Exception, request: WebRequest?): ResponseEntity<Any>{
-        log.error("Token Unauthorized -------> {}, {}, {}, {}", ex.message, HttpHeaders(), HttpStatus.UNAUTHORIZED, request.toString())
-        return ResponseEntity("Token unauthorized", HttpHeaders(), HttpStatus.UNAUTHORIZED)
+    fun håndtereTokenUnauthorizedException(exception: Exception, request: WebRequest?): ResponseEntity<Any>{
+        return logAndHandle(HttpStatus.UNAUTHORIZED, exception, request)
+    }
+
+    fun logAndHandle(status: HttpStatus, exception: Exception, request: WebRequest?): ResponseEntity<Any>{
+        logger.error("{} - {}, {}", status, exception, request.toString())
+        return ResponseEntity(status)
     }
 
 }
