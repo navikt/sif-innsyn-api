353c3dd27223200ad5fa8227cbe0fe4b11499879
Sander Opperud Odden <sander.opperud.odden@nav.no>
2020-07-29 15:37:30 +0200

    Fix

--- src/main/kotlin/no/nav/sifinnsynapi/ExceptionHandler.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/ExceptionHandler.kt
@@ -3,6 +3,7 @@ package no.nav.sifinnsynapi
 import no.nav.security.token.support.spring.validation.interceptor.JwtTokenUnauthorizedException
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -20,23 +21,25 @@ class ExceptionHandler {
 
     @ExceptionHandler(value = [Exception::class])
     fun håndtereGeneriskException(exception: Exception, request: WebRequest?): ResponseEntity<Any> {
-        return logAndHandle(HttpStatus.UNAUTHORIZED, exception, request)
+        log(HttpStatus.UNAUTHORIZED, exception, request)
+        return ResponseEntity("Generisk feilmelding", HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR)
     }
 
     @ExceptionHandler(value = [NoHandlerFoundException::class])
     @ResponseStatus(value= HttpStatus.NOT_FOUND)
     fun håndtere404Exception(exception: Exception, request: WebRequest?): ResponseEntity<Any> {
-        return logAndHandle(HttpStatus.UNAUTHORIZED, exception, request)
+        log(HttpStatus.UNAUTHORIZED, exception, request)
+        return ResponseEntity("404 - Not found feilmelding", HttpHeaders(), HttpStatus.NOT_FOUND)
     }
 
     @ExceptionHandler(value = [JwtTokenUnauthorizedException::class])
     fun håndtereTokenUnauthorizedException(exception: Exception, request: WebRequest?): ResponseEntity<Any>{
-        return logAndHandle(HttpStatus.UNAUTHORIZED, exception, request)
+        log(HttpStatus.UNAUTHORIZED, exception, request)
+        return ResponseEntity("Token unauthorized", HttpHeaders(), HttpStatus.UNAUTHORIZED)
     }
 
-    fun logAndHandle(status: HttpStatus, exception: Exception, request: WebRequest?): ResponseEntity<Any>{
+    fun log(status: HttpStatus, exception: Exception, request: WebRequest?){
         logger.error("{} - {}, {}", status, exception, request.toString())
-        return ResponseEntity(status)
     }
 
 }
