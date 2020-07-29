2bf110d14ef02e179c3e5385cb1b0fa3ac7dddaa
Sander Opperud Odden <sander.opperud.odden@nav.no>
2020-07-29 11:55:55 +0200

    Test exception handler

---
+++ src/main/kotlin/no/nav/sifinnsynapi/ExceptionHandler.kt
@@ -0,0 +1,25 @@
+package no.nav.sifinnsynapi
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.context.request.WebRequest
+
+@ControllerAdvice
+class ExceptionHandler {
+
+    companion object{
+        private val log: Logger = LoggerFactory.getLogger(ExceptionHandler::class.java)
+    }
+
+    @ExceptionHandler(value = [Exception::class])
+    fun handleAnyException(ex: Exception, request: WebRequest?): ResponseEntity<Any> {
+        log.error("TEST EXCEPTION -------> {}", ex.message)
+
+        return ResponseEntity(ex.message, HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+}
