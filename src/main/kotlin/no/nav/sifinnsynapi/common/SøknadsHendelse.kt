7e30b481e64ebb96eea1b2dd610c27897e5b6c6d
Sander Opperud Odden <sander.opperud.odden@nav.no>
2020-08-07 10:06:20 +0200

    Renamer OmsorgspengerutbetalingSNFHendelse til SøknadsHendelse

--- src/main/kotlin/no/nav/sifinnsynapi/common/SøknadsHendelse.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/common/SøknadsHendelse.kt
@@ -1,12 +1,9 @@
-package no.nav.sifinnsynapi.omsorgspenger
+package no.nav.sifinnsynapi.common
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.annotation.JsonRootName
-import no.nav.sifinnsynapi.common.JournalfortMelding
-import no.nav.sifinnsynapi.common.Metadata
 
-data class OmsorgspengerutbetalingSNFHendelse @JsonCreator constructor(
+data class SøknadsHendelse @JsonCreator constructor(
         @JsonProperty("metadata") val metadata: Metadata,
         @JsonProperty("melding") val melding: Map<String, Any>,
         @JsonProperty("journalførtMelding") val journalførtMelding: JournalfortMelding
--- src/main/kotlin/no/nav/sifinnsynapi/common/TopicEntry.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/common/TopicEntry.kt
@@ -2,8 +2,7 @@ package no.nav.sifinnsynapi.common
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
-import no.nav.sifinnsynapi.omsorgspenger.OmsorgspengerutbetalingSNFHendelse
 
 data class TopicEntry @JsonCreator constructor(
-        @JsonProperty("data") val data: OmsorgspengerutbetalingSNFHendelse
+        @JsonProperty("data") val data: SøknadsHendelse
 )
