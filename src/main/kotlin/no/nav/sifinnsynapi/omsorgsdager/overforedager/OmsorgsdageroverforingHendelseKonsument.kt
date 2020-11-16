c22987a92ba215eefd07a6d77e76e7021007f9f5
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2020-11-16 16:16:08 +0100

    Legger til forventet type på payload i lytter.
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

--- src/main/kotlin/no/nav/sifinnsynapi/omsorgsdager/overforedager/OmsorgsdageroverforingHendelseKonsument.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/omsorgsdager/overforedager/OmsorgsdageroverforingHendelseKonsument.kt
@@ -1,24 +1,13 @@
 package no.nav.sifinnsynapi.omsorgsdager.overforedager
 
-import no.nav.sifinnsynapi.common.*
-import no.nav.sifinnsynapi.dittnav.PleiepengerDittnavBeskjedProperties
-import no.nav.sifinnsynapi.pleiepenger.syktbarn.PleiepengerSyktBarnHendelseKonsument.Companion.Keys.AKTØR_ID
-import no.nav.sifinnsynapi.pleiepenger.syktbarn.PleiepengerSyktBarnHendelseKonsument.Companion.Keys.FØDSELSNUMMER
-import no.nav.sifinnsynapi.pleiepenger.syktbarn.PleiepengerSyktBarnHendelseKonsument.Companion.Keys.MOTTATT
-import no.nav.sifinnsynapi.pleiepenger.syktbarn.PleiepengerSyktBarnHendelseKonsument.Companion.Keys.SØKER
-import no.nav.sifinnsynapi.pleiepenger.syktbarn.PleiepengerSyktBarnHendelseKonsument.Companion.Keys.SØKNAD_ID
-import no.nav.sifinnsynapi.soknad.Søknad
-import no.nav.sifinnsynapi.soknad.SøknadDAO
+import no.nav.k9.rapid.losning.OverføreOmsorgsdagerLøsning
 import no.nav.sifinnsynapi.soknad.SøknadRepository
-import org.json.JSONObject
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.time.ZonedDateTime
-import java.util.*
 
 @Service
 class OmsorgsdagerOverforingHendelseKonsument(
@@ -28,14 +17,6 @@ class OmsorgsdagerOverforingHendelseKonsument(
     companion object {
         private val logger = LoggerFactory.getLogger(OmsorgsdagerOverforingHendelseKonsument::class.java)
         private val YTELSE = "'omsorgsdager - overføring'"
-
-        internal object Keys {
-            const val SØKNAD_ID = "søknadId"
-            const val SØKER = "søker"
-            const val AKTØR_ID = "aktørId"
-            const val MOTTATT = "mottatt"
-            const val FØDSELSNUMMER = "fødselsnummer"
-        }
     }
 
     @Transactional
@@ -47,9 +28,10 @@ class OmsorgsdagerOverforingHendelseKonsument(
             autoStartup = "#{'\${topic.listener.omd-overforing.bryter}'}"
     )
     fun konsumer(
-            @Payload hendelse: Any // TODO: Oppdatere med riktig type
+            @Payload hendelse: OverføreOmsorgsdagerLøsning
     ) {
-        logger.info("Konsumerer hendelse om $YTELSE")
+        val (id, løsning) = hendelse
+        logger.info("Mottatt løsning: {}", løsning)
     }
 }
 
