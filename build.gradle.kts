0c4ad447887cb1d4c1c1b9f80add02f227e5fc58
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2020-11-16 16:12:01 +0100

    Oppgraderer java versjon til 12
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 group = "no.nav"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_12
 
 configurations {
     compileOnly {
