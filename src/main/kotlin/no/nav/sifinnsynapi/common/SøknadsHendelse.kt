3c59cf0acf39575537bd5aaae8768f5520aa0b3c
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2020-11-18 13:11:38 +0100

    Legger til alias for melding.
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

--- src/main/kotlin/no/nav/sifinnsynapi/common/SøknadsHendelse.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/common/SøknadsHendelse.kt
@@ -1,11 +1,12 @@
 package no.nav.sifinnsynapi.common
 
+import com.fasterxml.jackson.annotation.JsonAlias
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class SøknadsHendelse @JsonCreator constructor(
         @JsonProperty("metadata") val metadata: Metadata,
-        @JsonProperty("melding") val melding: Map<String, Any>,
+        @JsonAlias("meldingV1") @JsonProperty("melding") val melding: Map<String, Any>,
         @JsonProperty("pdfDokument") val pdfDokument: ByteArray? =  null,
         @JsonProperty("journalførtMelding") val journalførtMelding: JournalfortMelding
 )
