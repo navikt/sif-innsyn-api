11a310a51f82a3ff8e8ad4fe834e8fcde3120f54
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2020-11-19 14:02:55 +0100

    Overstyrer tostring dor å skjule søkersFødselsnummer.
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

--- src/main/kotlin/no/nav/sifinnsynapi/dittnav/K9Beskjed.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/dittnav/K9Beskjed.kt
@@ -10,4 +10,8 @@ data class K9Beskjed(
         val dagerSynlig: Long,
         val søkerFødselsnummer: String,
         val eventId: String
-)
+) {
+    override fun toString(): String {
+        return "K9Beskjed(metadata=$metadata, grupperingsId='$grupperingsId', tekst='$tekst', link='$link', dagerSynlig=$dagerSynlig, søkerFødselsnummer='***********', eventId='$eventId')"
+    }
+}
