b8c4d1ed8d287b48fa639385982a460648777868
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2021-06-10 11:16:35 +0200

    Refaktorerer og aiven og onprem kafka config.
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

--- src/main/kotlin/no/nav/sifinnsynapi/config/AivenKafkaConfig.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/config/AivenKafkaConfig.kt
@@ -2,104 +2,44 @@ package no.nav.sifinnsynapi.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import no.nav.sifinnsynapi.config.CommonKafkaConfig.Companion.configureConcurrentKafkaListenerContainerFactory
+import no.nav.sifinnsynapi.config.CommonKafkaConfig.Companion.consumerFactory
+import no.nav.sifinnsynapi.config.CommonKafkaConfig.Companion.kafkaTemplate
+import no.nav.sifinnsynapi.config.CommonKafkaConfig.Companion.kafkaTransactionManager
+import no.nav.sifinnsynapi.config.CommonKafkaConfig.Companion.producerFactory
 import no.nav.sifinnsynapi.soknad.SøknadRepository
-import org.apache.kafka.clients.CommonClientConfigs
-import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.clients.producer.ProducerConfig
-import org.apache.kafka.common.config.SslConfigs
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.io.Resource
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
-import org.springframework.kafka.core.*
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
 import org.springframework.kafka.transaction.KafkaTransactionManager
-
 
 @Configuration
 class AivenKafkaConfig(
     private val objectMapper: ObjectMapper,
     private val søknadRepository: SøknadRepository,
-    @Value("\${kafka.aiven.servers}") private val bootstrapServers: String,
-    @Value("\${kafka.aiven.properties.security.protocol:#{null}}") private val securityProtocol: String?,
-    @Value("\${kafka.aiven.properties.ssl.truststore-location:#{null}}") private val trustStoreLocation: Resource?,
-    @Value("\${kafka.aiven.properties.ssl.truststore-password:#{null}}") private val trustStorePassword: String?,
-    @Value("\${kafka.aiven.properties.ssl.truststore-type:#{null}}") private val trustStoreType: String?,
-    @Value("\${kafka.aiven.properties.ssl.keystore-location:#{null}}") private val keyStoreLocation: Resource?,
-    @Value("\${kafka.aiven.properties.ssl.keystore-password:#{null}}") private val keystorePassword: String?,
-    @Value("\${kafka.aiven.properties.ssl.keystore-type:#{null}}") private val keystoreType: String?,
-    @Value("\${kafka.aiven.consumer.enable-auto-commit}") private val enableAutoCommit: Boolean,
-    @Value("\${kafka.aiven.consumer.group-id}") private val groupId: String,
-    @Value("\${kafka.aiven.consumer.auto-offset-reset}") private val autoOffsetReset: String,
-    @Value("\${kafka.aiven.consumer.isolation-level}") private val isolationLevel: String,
-    @Value("\${kafka.aiven.consumer.retry-interval}") private val retryInterval: Long,
-    @Value("\${kafka.aiven.consumer.key-deserializer}") private val keyDeserializer: String,
-    @Value("\${kafka.aiven.consumer.value-deserializer}") private val valueDeserializer: String,
-    @Value("\${kafka.aiven.producer.client-id}") private val clientId: String,
-    @Value("\${kafka.aiven.producer.key-serializer}") private val keySerializer: String,
-    @Value("\${kafka.aiven.producer.value-serializer}") private val valueSerializer: String,
-    @Value("\${kafka.aiven.producer.transaction-id-prefix}") private val transactionIdPrefix: String,
-    @Value("\${kafka.aiven.producer.retries}") private val retries: Int
+    private val kafkaClusterProperties: KafkaClusterProperties
 ) {
 
     companion object {
         private val logger = LoggerFactory.getLogger(AivenKafkaConfig::class.java)
     }
 
-    fun commonConfig() = mutableMapOf<String, Any>().apply {
-        put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
-    } + securityConfig()
-
-    fun securityConfig() = mutableMapOf<String, Any>().apply {
-        put(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, "") // Disable server host name verification
-        securityProtocol?.let { put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, it) }
-        trustStoreLocation?.let { put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, it.file.absolutePath) }
-        trustStorePassword?.let {  put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, it)  }
-        trustStoreType?.let { put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, it) }
-        keyStoreLocation?.let { put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, it.file.absolutePath) }
-        keystorePassword?.let { put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, it) }
-        keystoreType?.let { put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, it) }
-    }
+    @Bean
+    fun aivenConsumerFactory(): ConsumerFactory<String, String> = consumerFactory(kafkaClusterProperties.aiven)
 
     @Bean
-    fun aivenConsumerFactory(): ConsumerFactory<String, String> {
-        val consumerProperties = mutableMapOf<String, Any>(
-            ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG to enableAutoCommit,
-            ConsumerConfig.GROUP_ID_CONFIG to groupId,
-            ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to autoOffsetReset,
-            ConsumerConfig.ISOLATION_LEVEL_CONFIG to isolationLevel,
-            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to keyDeserializer,
-            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to valueDeserializer
-        ) + commonConfig()
-
-        return DefaultKafkaConsumerFactory(consumerProperties)
-    }
+    fun aivenProducerFactory(): ProducerFactory<String, String> = producerFactory(kafkaClusterProperties.aiven)
 
     @Bean
-    fun aivenProducerFactory(): ProducerFactory<String, String> {
-        val producerProperties = mutableMapOf<String, Any>(
-            ProducerConfig.CLIENT_ID_CONFIG to clientId,
-            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to keySerializer,
-            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to valueSerializer,
-            ProducerConfig.RETRIES_CONFIG to retries,
-            ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG to true, // Den sikrer rekkefølge
-            ProducerConfig.ACKS_CONFIG to "all" // Den sikrer at data ikke mistes
-        ) + commonConfig()
-
-        val factory = DefaultKafkaProducerFactory<String, String>(producerProperties)
-        factory.setTransactionIdPrefix(transactionIdPrefix)
-        return factory
-    }
-
-    @Bean
-    fun aivenKafkaTemplate(aivenProducerFactory: ProducerFactory<String, String>) = KafkaTemplate(aivenProducerFactory)
+    fun aivenKafkaTemplate(aivenProducerFactory: ProducerFactory<String, String>) =
+        kafkaTemplate(aivenProducerFactory, kafkaClusterProperties.aiven)
 
     @Bean
     fun aivenKafkaTransactionManager(aivenProducerFactory: ProducerFactory<String, String>) =
-        KafkaTransactionManager(aivenProducerFactory).apply {
-            setTransactionIdPrefix(transactionIdPrefix)
-        }
+        kafkaTransactionManager(aivenProducerFactory, kafkaClusterProperties.aiven)
 
     @Bean
     fun aivenKafkaJsonListenerContainerFactory(
@@ -107,11 +47,11 @@ class AivenKafkaConfig(
         aivenKafkaTemplate: KafkaTemplate<String, String>,
         aivenKafkaTransactionManager: KafkaTransactionManager<String, String>
     ): ConcurrentKafkaListenerContainerFactory<String, String> = configureConcurrentKafkaListenerContainerFactory(
-        clientId = groupId,
+        clientId = kafkaClusterProperties.aiven.consumer.groupId,
         consumerFactory = aivenConsumerFactory,
         kafkaTemplate = aivenKafkaTemplate,
         transactionManager = aivenKafkaTransactionManager,
-        retryInterval = retryInterval,
+        retryInterval = kafkaClusterProperties.aiven.consumer.retryInterval,
         objectMapper = objectMapper,
         søknadRepository = søknadRepository,
         logger = logger
--- src/main/kotlin/no/nav/sifinnsynapi/config/OnpremKafkaConfig.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/config/OnpremKafkaConfig.kt
@@ -22,83 +22,28 @@ import org.springframework.kafka.transaction.KafkaTransactionManager
 class OnpremKafkaConfig(
     private val objectMapper: ObjectMapper,
     private val søknadRepository: SøknadRepository,
-    @Value("\${kafka.onprem.servers}") private val bootstrapServers: String,
-    @Value("\${kafka.onprem.properties.security.protocol:#{null}}") private val securityProtocol: String?,
-    @Value("\${kafka.onprem.properties.sasl.mechanism:#{null}}") private val saslMechanism: String?,
-    @Value("\${kafka.onprem.properties.sasl.jaas-config:#{null}}") private val jaasConfig: String?,
-    @Value("\${kafka.onprem.properties.ssl.truststore-location:#{null}}") private val trustStoreLocation: Resource?,
-    @Value("\${kafka.onprem.properties.ssl.truststore-password:#{null}}") private val trustStorePassword: String?,
-    @Value("\${kafka.onprem.properties.ssl.truststore-type:#{null}}") private val trustStoreType: String?,
-    @Value("\${kafka.onprem.consumer.enable-auto-commit}") private val enableAutoCommit: Boolean,
-    @Value("\${kafka.onprem.consumer.group-id}") private val groupId: String,
-    @Value("\${kafka.onprem.consumer.auto-offset-reset}") private val autoOffsetReset: String,
-    @Value("\${kafka.onprem.consumer.isolation-level}") private val isolationLevel: String,
-    @Value("\${kafka.onprem.consumer.retry-interval}") private val retryInterval: Long,
-    @Value("\${kafka.onprem.consumer.key-deserializer}") private val keyDeserializer: String,
-    @Value("\${kafka.onprem.consumer.value-deserializer}") private val valueDeserializer: String,
-    @Value("\${kafka.onprem.producer.client-id}") private val clientId: String,
-    @Value("\${kafka.onprem.producer.key-serializer}") private val keySerializer: String,
-    @Value("\${kafka.onprem.producer.value-serializer}") private val valueSerializer: String,
-    @Value("\${kafka.onprem.producer.transaction-id-prefix}") private val transactionIdPrefix: String,
-    @Value("\${kafka.onprem.producer.retries}") private val retries: Int
+    private val kafkaClusterProperties: KafkaClusterProperties,
 ) {
 
     companion object {
         private val logger = LoggerFactory.getLogger(OnpremKafkaConfig::class.java)
     }
 
-    fun commonConfig() = mutableMapOf<String, Any>().apply {
-        put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
-    } + securityConfig()
-
-    fun securityConfig() = mutableMapOf<String, Any>().apply {
-        securityProtocol?.let { put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, it) }
-        saslMechanism?.let { put(SaslConfigs.SASL_MECHANISM, it) }
-        jaasConfig?.let { put(SaslConfigs.SASL_JAAS_CONFIG, it) }
-        trustStoreLocation?.let { put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, it.file.absolutePath) }
-        trustStorePassword?.let { put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, it) }
-        trustStoreType?.let { put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, it) }
-    }
+    @Bean
+    fun onpremConsumerFactory(): ConsumerFactory<String, String> =
+        CommonKafkaConfig.consumerFactory(kafkaClusterProperties.onprem)
 
     @Bean
-    fun onpremConsumerFactory(): ConsumerFactory<String, String> {
-        val consumerProperties = mutableMapOf<String, Any>(
-            ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG to enableAutoCommit,
-            ConsumerConfig.GROUP_ID_CONFIG to groupId,
-            ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to autoOffsetReset,
-            ConsumerConfig.ISOLATION_LEVEL_CONFIG to isolationLevel,
-            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to keyDeserializer,
-            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to valueDeserializer
-        ) + commonConfig()
-
-        return DefaultKafkaConsumerFactory(consumerProperties)
-    }
-
-    @Bean
-    fun onpremProducerFactory(): ProducerFactory<String, String> {
-        val producerProperties = mutableMapOf<String, Any>(
-            ProducerConfig.CLIENT_ID_CONFIG to clientId,
-            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to keySerializer,
-            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to valueSerializer,
-            ProducerConfig.RETRIES_CONFIG to retries
-        ) + commonConfig()
-
-        val factory = DefaultKafkaProducerFactory<String, String>(producerProperties)
-        factory.setTransactionIdPrefix(transactionIdPrefix)
-        return factory
-    }
+    fun onpremProducerFactory(): ProducerFactory<String, String> =
+        CommonKafkaConfig.producerFactory(kafkaClusterProperties.onprem)
 
     @Bean
     fun onpremKafkaTemplate(onpremProducerFactory: ProducerFactory<String, String>) =
-        KafkaTemplate(onpremProducerFactory).apply {
-            transactionIdPrefix = transactionIdPrefix
-        }
+        CommonKafkaConfig.kafkaTemplate(onpremProducerFactory, kafkaClusterProperties.onprem)
 
     @Bean
     fun onpremKafkaTransactionManager(onpremProducerFactory: ProducerFactory<String, String>) =
-        KafkaTransactionManager(onpremProducerFactory).apply {
-            setTransactionIdPrefix(transactionIdPrefix)
-        }
+        CommonKafkaConfig.kafkaTransactionManager(onpremProducerFactory, kafkaClusterProperties.onprem)
 
     @Bean
     fun onpremKafkaJsonListenerContainerFactory(
@@ -106,11 +51,11 @@ class OnpremKafkaConfig(
         onpremKafkaTemplate: KafkaTemplate<String, String>,
         onpremKafkaTransactionManager: KafkaTransactionManager<String, String>,
     ): ConcurrentKafkaListenerContainerFactory<String, String> = configureConcurrentKafkaListenerContainerFactory(
-        clientId = groupId,
+        clientId = kafkaClusterProperties.aiven.consumer.groupId,
         consumerFactory = onpremConsumerFactory,
         transactionManager = onpremKafkaTransactionManager,
         kafkaTemplate = onpremKafkaTemplate,
-        retryInterval = retryInterval,
+        retryInterval = kafkaClusterProperties.aiven.consumer.retryInterval,
         objectMapper = objectMapper,
         søknadRepository = søknadRepository,
         logger = logger
