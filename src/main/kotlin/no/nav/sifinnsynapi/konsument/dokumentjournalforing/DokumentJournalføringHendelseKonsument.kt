8c40c6a59c36ef9607a2d53f87e29a59eb33593d
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2021-06-17 11:20:55 +0200

    Fikser lesing av melding
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

--- src/main/kotlin/no/nav/sifinnsynapi/konsument/dokumentjournalforing/DokumentJournalføringHendelseKonsument.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/konsument/dokumentjournalforing/DokumentJournalføringHendelseKonsument.kt
@@ -1,13 +1,15 @@
 package no.nav.sifinnsynapi.konsument.dokumentjournalforing
 
-import org.json.JSONObject
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Service
 
 @Service
-class DokumentJournalføringHendelseKonsument {
+class DokumentJournalføringHendelseKonsument(
+    private val objectMapper: ObjectMapper
+) {
 
     companion object {
         private val logger = LoggerFactory.getLogger(DokumentJournalføringHendelseKonsument::class.java)
@@ -23,7 +25,7 @@ class DokumentJournalføringHendelseKonsument {
     fun konsumer(
         @Payload melding: String
     ) {
-        val melding = JSONObject(melding)
-        logger.info("Mottok hendelse om dokumentjournalføring: {}", melding.toString(2))
+        val jsonMelding = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(melding)
+        logger.info("Mottok hendelse om dokumentjournalføring: {}", jsonMelding)
     }
 }
