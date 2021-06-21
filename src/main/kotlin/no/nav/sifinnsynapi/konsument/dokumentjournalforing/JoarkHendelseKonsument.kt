459fd7f0008c92fd7f99a42b3ef2f38432b1fff9
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2021-06-21 15:42:22 +0200

    Tester transaction rollback og retrymekanisme
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

--- src/main/kotlin/no/nav/sifinnsynapi/konsument/dokumentjournalforing/JoarkHendelseKonsument.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/konsument/dokumentjournalforing/JoarkHendelseKonsument.kt
@@ -2,6 +2,7 @@ package no.nav.sifinnsynapi.konsument.dokumentjournalforing
 
 import kotlinx.coroutines.runBlocking
 import no.nav.joarkjournalfoeringhendelser.JournalfoeringHendelseRecord
+import no.nav.sifinnsynapi.config.TxConfiguration.Companion.TRANSACTION_MANAGER
 import no.nav.sifinnsynapi.saf.SafService
 import no.nav.sifinnsynapi.soknad.SøknadRepository
 import no.nav.sifinnsynapi.util.Constants
@@ -11,7 +12,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Service
-import java.lang.IllegalStateException
+import org.springframework.transaction.annotation.Transactional
 
 
 @Service
@@ -24,6 +25,7 @@ class JoarkHendelseKonsument(
         private val logger = LoggerFactory.getLogger(JoarkHendelseKonsument::class.java)
     }
 
+    @Transactional(TRANSACTION_MANAGER)
     @KafkaListener(
         topics = ["#{'\${topic.listener.dok-journalfoering-v1.navn}'}"],
         id = "#{'\${topic.listener.dok-journalfoering-v1.id}'}",
@@ -52,6 +54,8 @@ class JoarkHendelseKonsument(
 
             val oppdatertSøknad = søknadRepository.save(søknad.copy(saksId = fagsakId))
             logger.info("Søknad oppdatert med saksId: {}", oppdatertSøknad)
+
+            throw IllegalStateException("Tester transaction rollback og retrymekanisme")
         }
     }
 }
