ecec653424b5a2fd214774d48d7549e58efdce96
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2021-06-21 15:29:38 +0200

    Oppdaterer søknad med saksId
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

--- src/main/kotlin/no/nav/sifinnsynapi/konsument/dokumentjournalforing/JoarkHendelseKonsument.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/konsument/dokumentjournalforing/JoarkHendelseKonsument.kt
@@ -3,6 +3,7 @@ package no.nav.sifinnsynapi.konsument.dokumentjournalforing
 import kotlinx.coroutines.runBlocking
 import no.nav.joarkjournalfoeringhendelser.JournalfoeringHendelseRecord
 import no.nav.sifinnsynapi.saf.SafService
+import no.nav.sifinnsynapi.soknad.SøknadRepository
 import no.nav.sifinnsynapi.util.Constants
 import no.nav.sifinnsynapi.util.MDCUtil
 import org.apache.kafka.clients.consumer.ConsumerRecord
@@ -10,11 +11,13 @@ import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Service
+import java.lang.IllegalStateException
 
 
 @Service
 class JoarkHendelseKonsument(
-    private val safService: SafService
+    private val safService: SafService,
+    private val søknadRepository: SøknadRepository
 ) {
 
     companion object {
@@ -31,12 +34,24 @@ class JoarkHendelseKonsument(
     fun konsumer(
         @Payload cr: ConsumerRecord<Long, JournalfoeringHendelseRecord>
     ) {
-        logger.info("Mottatt journalføringshendelse med status: {}", cr.value().hendelsesType)
+        val hendelse = cr.value()
+        logger.info("Mottatt journalføringshendelse med status: {}", hendelse.hendelsesType)
+
+        val journalpostId = "${hendelse.journalpostId}"
 
         runBlocking {
-            val journalpostinfo = safService.hentJournalpostinfo("${cr.value().journalpostId}")
-            MDCUtil.toMDC(Constants.K9_SAK_ID, journalpostinfo.sak!!.fagsakId)
-            logger.info("Hentet journalpostInfo: {}", journalpostinfo)
+            logger.info("Slår opp journalpostinfo...")
+            val journalpostinfo = safService.hentJournalpostinfo(journalpostId)
+            val fagsakId = journalpostinfo.sak!!.fagsakId
+            MDCUtil.toMDC(Constants.K9_SAK_ID, fagsakId)
+            logger.info("JournalpostInfo hentet.")
+
+            logger.info("Oppdaterer søknad med saksId...")
+            val søknad = søknadRepository.findByJournalpostId(journalpostId)
+                ?: throw IllegalStateException("Søknad med journalpostId ble ikke funnet i db: $journalpostId")
+
+            val oppdatertSøknad = søknadRepository.save(søknad.copy(saksId = fagsakId))
+            logger.info("Søknad oppdatert med saksId: {}", oppdatertSøknad)
         }
     }
 }
