7894214df1f0becc079f05abe424a82ddbea1177
SanderOpperud <60642441+SanderOpperud@users.noreply.github.com>
2021-08-11 13:12:36 +0200

    Fjerner konsumering av K9-ettersending som er flyttet til Aiven. (#132)
    Fjerner tester og andre referanser til ettersending.

--- src/test/kotlin/no/nav/sifinnsynapi/utils/SøknadUtils.kt
+++ src/test/kotlin/no/nav/sifinnsynapi/utils/SøknadUtils.kt
@@ -6,7 +6,6 @@ import no.nav.sifinnsynapi.common.JournalfortMelding
 import no.nav.sifinnsynapi.common.Metadata
 import no.nav.sifinnsynapi.common.SøknadsHendelse
 import no.nav.sifinnsynapi.common.TopicEntry
-import no.nav.sifinnsynapi.konsument.ettersending.K9EttersendingKonsument
 import no.nav.sifinnsynapi.soknad.SøknadDTO
 import java.time.ZonedDateTime
 import java.util.*
@@ -36,29 +35,6 @@ fun defaultHendelse(
                 "fødselsnummer" to "1234567",
                 "aktørId" to "123456"
             )
-        ),
-        journalførtMelding = JournalfortMelding(
-            journalpostId = "$journalpostId"
-        )
-    )
-)
-
-fun defaultHendelseK9Ettersending(
-    søknadIdKey: String = "soknadId",
-    søknadId: UUID = UUID.randomUUID(),
-    journalpostId: String = "123456789",
-    søknadstype: K9EttersendingKonsument.Companion.Søknadstype
-) = TopicEntry(
-    data = SøknadsHendelse(
-        metadata = defaultMetadata,
-        melding = mapOf(
-            "$søknadIdKey" to "$søknadId",
-            "mottatt" to ZonedDateTime.now(),
-            "søker" to mapOf(
-                "fødselsnummer" to "1234567",
-                "aktørId" to "123456"
-            ),
-            "søknadstype" to "${søknadstype.name}"
         ),
         journalførtMelding = JournalfortMelding(
             journalpostId = "$journalpostId"
