7159fd7cfbb2aa9884b68342a3e6582c29801126
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2021-08-20 11:43:27 +0200

    Logger konfigurering av tokenx klient for safselvbetjening.
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

--- src/main/kotlin/no/nav/sifinnsynapi/config/TokenClientConfig.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/config/TokenClientConfig.kt
@@ -4,6 +4,7 @@ import no.nav.security.token.support.client.core.ClientProperties
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
+import org.slf4j.LoggerFactory
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -17,6 +18,10 @@ import org.springframework.web.client.RestTemplate
 @Configuration
 class TokenClientConfig {
 
+    private companion object {
+        private val logger = LoggerFactory.getLogger(TokenClientConfig::class.java)
+    }
+
     @Bean
     fun tokenXSafSelvbetjeningClient(
         restTemplateBuilder: RestTemplateBuilder,
@@ -26,6 +31,8 @@ class TokenClientConfig {
         val clientProperties: ClientProperties =
             clientConfigurationProperties.registration["tokenx-safselvbetjening"]
                 ?: throw RuntimeException("could not find oauth2 client config for tokenx-safselvbetjening")
+
+        logger.info("Konfigurerer opp tokenx klient for safselvbetjening.")
         return restTemplateBuilder
             .additionalInterceptors(bearerTokenInterceptor(clientProperties, oAuth2AccessTokenService))
             .build()
