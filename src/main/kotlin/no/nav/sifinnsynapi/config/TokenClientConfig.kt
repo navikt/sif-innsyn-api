32331a709d7320fb5fbf620a4045c7fea34afe2c
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2021-08-20 11:33:28 +0200

    Legger til tokenx klientkonfigurasjon for safselvbetjening.
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

---
+++ src/main/kotlin/no/nav/sifinnsynapi/config/TokenClientConfig.kt
@@ -0,0 +1,44 @@
+package no.nav.sifinnsynapi.config
+
+import no.nav.security.token.support.client.core.ClientProperties
+import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
+import no.nav.security.token.support.client.spring.ClientConfigurationProperties
+import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpRequest
+import org.springframework.http.client.ClientHttpRequestExecution
+import org.springframework.http.client.ClientHttpRequestInterceptor
+import org.springframework.web.client.RestTemplate
+
+
+@EnableOAuth2Client(cacheEnabled = true)
+@Configuration
+class TokenClientConfig {
+
+    @Bean
+    fun tokenXSafSelvbetjeningClient(
+        restTemplateBuilder: RestTemplateBuilder,
+        clientConfigurationProperties: ClientConfigurationProperties,
+        oAuth2AccessTokenService: OAuth2AccessTokenService
+    ): RestTemplate? {
+        val clientProperties: ClientProperties =
+            clientConfigurationProperties.registration["tokenx-safselvbetjening"]
+                ?: throw RuntimeException("could not find oauth2 client config for tokenx-safselvbetjening")
+        return restTemplateBuilder
+            .additionalInterceptors(bearerTokenInterceptor(clientProperties, oAuth2AccessTokenService))
+            .build()
+    }
+
+    private fun bearerTokenInterceptor(
+        clientProperties: ClientProperties,
+        oAuth2AccessTokenService: OAuth2AccessTokenService
+    ): ClientHttpRequestInterceptor {
+        return ClientHttpRequestInterceptor { request: HttpRequest, body: ByteArray, execution: ClientHttpRequestExecution ->
+            val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
+            request.headers.setBearerAuth(response.accessToken)
+            execution.execute(request, body)
+        }
+    }
+}
