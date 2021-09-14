773954b14724c45c4985c39ba05ffa234887689c
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2021-09-14 08:12:06 +0200

    Integrerer mot k9-sak-innsyn-api for oppslag av søknadsopplysninger fra k9-sak. (#144)
    * Integrerer mot k9-sak-innsyn-api.
    
    - Legger til endepunkt for å hente søknader fra k9-sak-innsyn-api
    - Legger til k9-format:soknad da data hentet fra k9-sak-innsyn-api inneholder dette formatet.
    
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

---
+++ src/main/kotlin/no/nav/sifinnsynapi/k9sakinnsynapi/K9SakInnsynApiService.kt
@@ -0,0 +1,80 @@
+package no.nav.sifinnsynapi.k9sakinnsynapi
+
+import no.nav.k9.søknad.Søknad
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.http.HttpMethod
+import org.springframework.retry.annotation.Backoff
+import org.springframework.retry.annotation.Recover
+import org.springframework.retry.annotation.Retryable
+import org.springframework.stereotype.Service
+import org.springframework.web.client.HttpClientErrorException
+import org.springframework.web.client.HttpServerErrorException
+import org.springframework.web.client.ResourceAccessException
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.util.UriComponentsBuilder
+import java.util.*
+
+@Service
+@Retryable(
+    exclude = [HttpClientErrorException.Unauthorized::class, HttpClientErrorException.Forbidden::class, ResourceAccessException::class],
+    backoff = Backoff(
+        delayExpression = "\${spring.rest.retry.initialDelay}",
+        multiplierExpression = "\${spring.rest.retry.multiplier}",
+        maxDelayExpression = "\${spring.rest.retry.maxDelay}"
+    ),
+    maxAttemptsExpression = "\${spring.rest.retry.maxAttempts}"
+)
+class K9SakInnsynApiService(
+    @Qualifier("k9SakInnsynApiClient")
+    private val k9SakInnsynClient: RestTemplate
+) {
+    private companion object {
+        private val logger: Logger = LoggerFactory.getLogger(K9SakInnsynApiService::class.java)
+
+        val søknaddataUrl = UriComponentsBuilder
+            .fromUriString("/soknad")
+            .build()
+            .toUriString()
+    }
+
+    fun hentSøknader(): List<K9SakInnsynSøknad> {
+        val exchange = k9SakInnsynClient.exchange(
+            søknaddataUrl,
+            HttpMethod.GET,
+            null,
+            object : ParameterizedTypeReference<List<K9SakInnsynSøknad>>() {})
+        logger.info("Fikk response {} for oppslag av søknadsdata fra k9-sak-innsyn-api", exchange.statusCode)
+
+        return if (exchange.statusCode.is2xxSuccessful) {
+            exchange.body!!
+        } else {
+            logger.error("Henting av søknadsdata feilet med status: {}, og respons: {}", exchange.statusCode, exchange.body)
+            throw IllegalStateException("Feilet med henting av k9 søknadsdata.")
+        }
+    }
+
+    @Recover
+    private fun recover(error: HttpServerErrorException): K9SakInnsynSøknad? {
+        logger.error("Error response = '${error.responseBodyAsString}' fra '${søknaddataUrl}'")
+        throw IllegalStateException("Feilet med henting av k9 søknadsdata.")
+    }
+
+    @Recover
+    private fun recover(error: HttpClientErrorException): K9SakInnsynSøknad? {
+        logger.error("Error response = '${error.responseBodyAsString}' fra '${søknaddataUrl}'")
+        throw IllegalStateException("Feilet med henting av k9 søknadsdata.")
+    }
+
+    @Recover
+    private fun recover(error: ResourceAccessException): K9SakInnsynSøknad? {
+        logger.error("{}", error.message)
+        throw IllegalStateException("Timout ved henting av k9 søknadsdata.")
+    }
+}
+
+data class K9SakInnsynSøknad(
+    val søknad: Søknad
+)
---
+++ src/main/kotlin/no/nav/sifinnsynapi/k9sakinnsynapi/K9SakInnsynSøknadController.kt
@@ -0,0 +1,29 @@
+package no.nav.sifinnsynapi.k9sakinnsynapi
+
+import no.nav.security.token.support.core.api.Protected
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import no.nav.sifinnsynapi.Routes.K9_SAK_INNSYN
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus.OK
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@ProtectedWithClaims(issuer = "selvbetjening", claimMap = ["acr=Level4"])
+class K9SakInnsynSøknadController(
+        private val k9SakInnsynApiService: K9SakInnsynApiService
+) {
+    companion object {
+        val logger = LoggerFactory.getLogger(K9SakInnsynSøknadController::class.java)
+    }
+
+    @GetMapping(K9_SAK_INNSYN, produces = [MediaType.APPLICATION_JSON_VALUE])
+    @Protected
+    @ResponseStatus(OK)
+    fun hentSøknader(): List<K9SakInnsynSøknad>? {
+        logger.info("Forsøker å hente søknader...")
+        val søknader = k9SakInnsynApiService.hentSøknader()
+        logger.info("Fant {} søknader", søknader.size)
+        return søknader
+    }
+}
