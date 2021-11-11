f88959152aba5ccf2f56ac40834293076d16a2e1
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2021-11-11 11:14:37 +0100

    Sletter ubrukte klasser.
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

--- src/main/kotlin/no/nav/sifinnsynapi/dokument/DokumentRepository.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.sifinnsynapi.dokument
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.transaction.annotation.Transactional
-import java.util.*
-
-interface DokumentRepository: JpaRepository<DokumentDAO, UUID> {
-    fun findBySøknadId(søknadId: UUID): DokumentDAO?
-}
