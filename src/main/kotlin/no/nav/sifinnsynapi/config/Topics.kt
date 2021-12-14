d49005694cd01ef67bf421353073136d3cd485a8
Ramin Esfandiari <ramin.esfandiari@nav.no>
2021-12-14 10:45:53 +0100

    Migrerer til aapen-dok-journalfoering på aiven. (#187)
    - Fjerner onprem kafka konfig da dette ikke brukes lenger.
    
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

--- src/main/kotlin/no/nav/sifinnsynapi/config/Topics.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/config/Topics.kt
@@ -1,9 +1,8 @@
 package no.nav.sifinnsynapi.config
 
 object Topics {
-    const val K9_DITTNAV_VARSEL_BESKJED = "privat-k9-dittnav-varsel-beskjed"
     const val K9_DITTNAV_VARSEL_BESKJED_AIVEN = "dusseldorf.privat-k9-dittnav-varsel-beskjed"
     const val PP_SYKT_BARN = "dusseldorf.privat-pleiepengesoknad-cleanup"
     const val K9_ETTERSENDING = "dusseldorf.privat-k9-ettersending-cleanup"
-    const val AAPEN_DOK_JOURNALFØRING_V1 = "aapen-dok-journalfoering-v1-p"
+    const val AAPEN_DOK_JOURNALFØRING_V1 = "teamdokumenthandtering.aapen-dok-journalfoering"
 }
