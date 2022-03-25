f26348227d2112b25ab55b427c2e7a72d494cc15
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2022-03-25 16:05:09 +0100

    Disable default swagger-url
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

--- src/main/kotlin/no/nav/sifinnsynapi/config/SwaggerConfiguration.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/config/SwaggerConfiguration.kt
@@ -1,29 +1,23 @@
 package no.nav.sifinnsynapi.config
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
-import io.swagger.v3.oas.annotations.security.SecurityScheme
 import io.swagger.v3.oas.models.ExternalDocumentation
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
-import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.servers.Server
-import org.springframework.context.EnvironmentAware
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
-import org.springframework.core.env.Environment
-
 
 @Configuration
 @Profile("local", "dev-gcp")
-class SwaggerConfiguration : EnvironmentAware {
-    private var env: Environment? = null
+class SwaggerConfiguration {
 
     @Bean
     fun openAPI(): OpenAPI {
         return OpenAPI()
-            .addServersItem(Server().url("https://sif-innsyn-api.dev.nav.no/").description("Swagger Server"))
+            .addServersItem(
+                Server().url("https://sif-innsyn-api.dev.nav.no/").description("Swagger Server")
+            )
             .info(
                 Info()
                     .title("Sif Innsyn Api")
@@ -35,9 +29,5 @@ class SwaggerConfiguration : EnvironmentAware {
                     .description("sif-innsyn-api github repository")
                     .url("https://github.com/navikt/sif-innsyn-api")
             )
-    }
-
-    override fun setEnvironment(env: Environment) {
-        this.env = env
     }
 }
