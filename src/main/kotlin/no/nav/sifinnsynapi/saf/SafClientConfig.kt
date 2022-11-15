9f9021cd04055bc0067808b498944051bda4935f
Ramin Esfandiari <ramin.esfandiari@nav.no>
2022-11-15 08:47:50 +0100

    Erstatter STS med azure for systemkall mot SAF. (#272)
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

--- src/main/kotlin/no/nav/sifinnsynapi/saf/SafClientConfig.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/saf/SafClientConfig.kt
@@ -1,10 +1,11 @@
 package no.nav.sifinnsynapi.saf
 
 import com.expediagroup.graphql.client.spring.GraphQLWebClient
-import no.nav.sifinnsynapi.sts.STSClient
+import no.nav.security.token.support.client.core.ClientProperties
+import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
+import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import no.nav.sifinnsynapi.util.HttpHeaderConstants
 import no.nav.sifinnsynapi.util.MDCUtil
-import no.nav.sifinnsynapi.util.asAuthoriationHeader
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
@@ -18,13 +19,17 @@ import reactor.netty.http.client.HttpClientResponse
 
 @Configuration
 class SafClientConfig(
-    private val stsClient: STSClient,
-    @Value("\${no.nav.gateways.saf-base-url}") private val safBaseUrl: String
+    oauth2Config: ClientConfigurationProperties,
+    private val oAuth2AccessTokenService: OAuth2AccessTokenService,
+    @Value("\${no.nav.gateways.saf-base-url}") private val safBaseUrl: String,
 ) {
 
     private companion object {
         private val logger = LoggerFactory.getLogger(SafClientConfig::class.java)
     }
+
+    private val azureSafClientProperties = oauth2Config.registration["azure-saf"]
+        ?: throw RuntimeException("could not find oauth2 client config for azure-saf")
 
     @Bean("safClient")
     fun client() = GraphQLWebClient(
@@ -37,15 +42,25 @@ class SafClientConfig(
                             logger.info("{} {} {}", request.version(), request.method(), request.resourceUrl())
                         }
                         .doOnResponse { response: HttpClientResponse, _ ->
-                            logger.info("{} - {} {} {}", response.status().toString(), response.version(), response.method(), response.resourceUrl())
+                            logger.info(
+                                "{} - {} {} {}",
+                                response.status().toString(),
+                                response.version(),
+                                response.method(),
+                                response.resourceUrl()
+                            )
                         }
                 )
             )
             .defaultRequest {
-                it.header(AUTHORIZATION, stsClient.oicdToken().asAuthoriationHeader())
+                it.header(AUTHORIZATION, "Bearer ${accessToken(azureSafClientProperties)}")
                 val correlationId = MDCUtil.callIdOrNew()
                 it.header(HttpHeaderConstants.NAV_CALL_ID, correlationId)
                 it.header(HttpHeaderConstants.X_CORRELATION_ID, correlationId)
             }
     )
+
+    private fun accessToken(clientProperties: ClientProperties): String {
+        return oAuth2AccessTokenService.getAccessToken(clientProperties).accessToken
+    }
 }
