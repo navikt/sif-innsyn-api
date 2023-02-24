9a08d2eb97cfaed4a98fa553ce8e6355734d616e
Ramin Esfandiari <ramin.esfandiari@nav.no>
2023-02-24 13:53:29 +0100

    Legger til handler interceptor for å logge request og response. (#299)
    * Legger til handler interceptor for å logge request og response.
    
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
    
    * Refaktorer implementasjon til eget klasse.
    
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
    
    ---------
    
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

--- src/main/kotlin/no/nav/sifinnsynapi/config/WebMvcConfig.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/config/WebMvcConfig.kt
@@ -3,6 +3,7 @@ package no.nav.sifinnsynapi.config
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.SerializationFeature
+import no.nav.sifinnsynapi.filter.LoggerInterceptor
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
@@ -10,10 +11,12 @@ import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilde
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebMvcConfig(
+    private val loggerInterceptor: LoggerInterceptor,
     @Value("\${no.nav.security.cors.allowed-origins}") val allowedOrigins: String
 ) : WebMvcConfigurer {
 
@@ -49,5 +52,10 @@ class WebMvcConfig(
             )
             builder.propertyNamingStrategy(PropertyNamingStrategies.LOWER_CAMEL_CASE)
         }
+    }
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(loggerInterceptor)
+        super.addInterceptors(registry)
     }
 }
---
+++ src/main/kotlin/no/nav/sifinnsynapi/filter/LoggerInterceptor.kt
@@ -0,0 +1,39 @@
+package no.nav.sifinnsynapi.filter
+
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import no.nav.security.token.support.core.context.TokenValidationContextHolder
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import org.springframework.web.servlet.ModelAndView
+
+@Component
+class LoggerInterceptor(private val tokenValidationContextHolder: TokenValidationContextHolder) : HandlerInterceptor {
+    private companion object {
+        private val logger = LoggerFactory.getLogger(LoggerInterceptor::class.java)
+    }
+    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+        val jwtToken = tokenValidationContextHolder.tokenValidationContext.firstValidToken.orElse(null)
+        if (jwtToken !== null) {
+            logger.info("Issuer [${jwtToken.issuer}]")
+        }
+        val method = request.method
+        val requestURI = request.requestURI
+        logger.info("Request $method $requestURI")
+        return super.preHandle(request, response, handler)
+    }
+
+    override fun postHandle(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        modelAndView: ModelAndView?
+    ) {
+        val status = response.status
+        val method = request.method
+        val requestURI = request.requestURI
+        logger.info("Response $status $method $requestURI")
+        super.postHandle(request, response, handler, modelAndView)
+    }
+}
