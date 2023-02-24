4a5e35e5a4e48c99d7a95fb21ee209ab489f122d
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-02-24 12:03:46 +0100

    Legger til handler interceptor for å logge request og response.
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

--- src/main/kotlin/no/nav/sifinnsynapi/config/WebMvcConfig.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/config/WebMvcConfig.kt
@@ -3,13 +3,20 @@ package no.nav.sifinnsynapi.config
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.SerializationFeature
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import no.nav.security.token.support.core.jwt.JwtToken
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
+import org.springframework.web.servlet.HandlerInterceptor
+import org.springframework.web.servlet.ModelAndView
 import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
@@ -49,5 +56,35 @@ class WebMvcConfig(
             )
             builder.propertyNamingStrategy(PropertyNamingStrategies.LOWER_CAMEL_CASE)
         }
+    }
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(object : HandlerInterceptor {
+            override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+                val authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION)?.replace("Bearer ", "")
+                if (!authorizationHeader.isNullOrBlank()) {
+                    val issuer = JwtToken(authorizationHeader).issuer
+                    log.info("Issuer [$issuer]")
+                }
+                val method = request.method
+                val requestURI = request.requestURI
+                log.info("Request $method $requestURI")
+                return super.preHandle(request, response, handler)
+            }
+
+            override fun postHandle(
+                request: HttpServletRequest,
+                response: HttpServletResponse,
+                handler: Any,
+                modelAndView: ModelAndView?
+            ) {
+                val status = response.status
+                val method = request.method
+                val requestURI = request.requestURI
+                log.info("Response $status $method $requestURI")
+                super.postHandle(request, response, handler, modelAndView)
+            }
+        })
+        super.addInterceptors(registry)
     }
 }
