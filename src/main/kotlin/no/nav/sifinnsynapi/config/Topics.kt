eeed7aea06522fe6af499fa3c34f65a1bab35d96
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-03-21 13:42:09 +0100

    Konsumerer PSB cleanup meldinger fra ny topic.
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

--- src/main/kotlin/no/nav/sifinnsynapi/config/Topics.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/config/Topics.kt
@@ -2,7 +2,7 @@ package no.nav.sifinnsynapi.config
 
 object Topics {
     const val K9_DITTNAV_VARSEL_BESKJED_AIVEN = "dusseldorf.privat-k9-dittnav-varsel-beskjed"
-    const val PP_SYKT_BARN = "dusseldorf.privat-pleiepengesoknad-cleanup"
+    const val PP_SYKT_BARN = "dusseldorf.pp-sykt-barn-soknad-cleanup"
     const val PP_SYKT_BARN_ENDRINGSMELDING= "dusseldorf.privat-endringsmelding-pleiepenger-sykt-barn-cleanup"
     const val K9_ETTERSENDING = "dusseldorf.privat-k9-ettersending-cleanup"
     const val AAPEN_DOK_JOURNALFØRING_V1 = "teamdokumenthandtering.aapen-dok-journalfoering"
