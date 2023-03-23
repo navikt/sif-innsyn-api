b5200d68c5ac82a114e53f432e088d3e5073ce49
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-03-23 10:23:13 +0100

    Fikser navn på gjenglemt topic for ettersendelser.
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

--- src/main/kotlin/no/nav/sifinnsynapi/config/Topics.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/config/Topics.kt
@@ -4,6 +4,6 @@ object Topics {
     const val K9_DITTNAV_VARSEL_BESKJED_AIVEN = "dusseldorf.privat-k9-dittnav-varsel-beskjed"
     const val PP_SYKT_BARN = "dusseldorf.pp-sykt-barn-soknad-cleanup"
     const val PP_SYKT_BARN_ENDRINGSMELDING= "dusseldorf.privat-endringsmelding-pleiepenger-sykt-barn-cleanup"
-    const val K9_ETTERSENDING = "dusseldorf.privat-k9-ettersending-cleanup"
+    const val K9_ETTERSENDING = "dusseldorf.ettersendelse-cleanup"
     const val AAPEN_DOK_JOURNALFØRING_V1 = "teamdokumenthandtering.aapen-dok-journalfoering"
 }
