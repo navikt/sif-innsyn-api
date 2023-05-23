d7861a02310ff3a00a69f8d38ca651eea6f7aa9e
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-05-23 10:20:40 +0200

    Oppgraderer scala-library med sårbarhet.
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -123,6 +123,14 @@ dependencies {
 
     //Kafka
     implementation("org.springframework.kafka:spring-kafka")
+    constraints {
+        implementation("org.scala-lang:scala-library") {
+            because("org.apache.kafka:kafka_2.13:3.3.2 -> https://www.cve.org/CVERecord?id=CVE-2022-36944")
+            version {
+                require("2.13.9")
+            }
+        }
+    }
     implementation("io.confluent:kafka-connect-avro-converter:$confluentVersion")
     implementation("io.confluent:kafka-avro-serializer:$confluentVersion")
     testImplementation("org.springframework.kafka:spring-kafka-test")
