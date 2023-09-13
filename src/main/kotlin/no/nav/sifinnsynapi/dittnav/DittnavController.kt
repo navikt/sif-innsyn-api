fce7e80d53d1f18d99fd9a889a4e50326a4729d2
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-09-13 10:51:17 +0200

    Annoterer requestbody

--- src/main/kotlin/no/nav/sifinnsynapi/dittnav/DittnavController.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/dittnav/DittnavController.kt
@@ -1,13 +1,14 @@
 package no.nav.sifinnsynapi.dittnav
 
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController("/dittnav")
 class DittnavController(private val dittnavService: DittnavService) {
 
     @PostMapping("/microfrontend/toggle")
-    fun toggleMicrofrontend(k9Microfrontend: K9Microfrontend) {
+    fun toggleMicrofrontend(@RequestBody k9Microfrontend: K9Microfrontend) {
         dittnavService.toggleMicrofrontend(k9Microfrontend)
     }
 }
