297131da4982b22b5eaf76188e2621e38dc2a040
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-09-13 11:04:14 +0200

    Legger til response status

--- src/main/kotlin/no/nav/sifinnsynapi/dittnav/DittnavController.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/dittnav/DittnavController.kt
@@ -1,13 +1,16 @@
 package no.nav.sifinnsynapi.dittnav
 
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController("/dittnav")
 class DittnavController(private val dittnavService: DittnavService) {
 
     @PostMapping("/microfrontend/toggle")
+    @ResponseStatus(HttpStatus.OK)
     fun toggleMicrofrontend(@RequestBody k9Microfrontend: K9Microfrontend) {
         dittnavService.toggleMicrofrontend(k9Microfrontend)
     }
