4e836d42ec9b89a103d94cc8621723e4e95fd2b9
Morten Osvik <33130876+mortenoosvik@users.noreply.github.com>
2026-04-13 15:23:16 +0200

    Downgrade Spring boot (#685)
    ### **Behov / Bakgrunn**
    Ny version av spring boot gjør at vi bruker jackson 3, noe vi ikke er
    rigget for helt ennå.
    
    ### **Løsning**
    Downgrader, tilsvarende som
    https://github.com/navikt/k9-sak-innsyn-api/pull/503

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     kotlin("jvm") version "2.3.20"
     kotlin("plugin.spring") version "2.3.20"
     kotlin("plugin.jpa") version "2.3.20"
-    id("org.springframework.boot") version "4.0.5"
+    id("org.springframework.boot") version "4.0.3"
     id("io.spring.dependency-management") version "1.1.7"
     id("com.expediagroup.graphql") version "9.1.0"
     id("org.sonarqube") version "7.2.3.7755"
